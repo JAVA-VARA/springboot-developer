dddc2f4900c07cdab75f1fbe82de984afcfa90b1
sj <sj.youns1027@gmail.com>
2023-06-30 16:35:44 +0900

    6단원 종료. 최종 병합

--- src/main/java/sjspring/shop/springbootdeveloper/domain/Article.java
+++ src/main/java/sjspring/shop/springbootdeveloper/domain/Article.java
@@ -1,10 +1,7 @@
 package sjspring.shop.springbootdeveloper.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 
