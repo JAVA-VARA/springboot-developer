8b0dd354e402331f8b5422726ea191ac681823bb
sj <sj.youns1027@gmail.com>
2023-07-04 14:33:58 +0900

    "7단원 블로그 화면 구성하기 종료/최종 병합"
    [연습용 예제 추가]
    - ExampleController.java
    => Get 요청이 오면 데이터를 HTML로 넘겨주는 컨트롤러
    
    -example.html
    => 예제 뷰(view) 작성. controller로부터 넘겨 받은데이터를 웹페이지에 구현
    ____________________________________________
    [블로그 화면 구성]
    1. 추가
    - BlogViewController.java (controller)
    => /articles, newArticles 의 Get 요청 처리.
    
    - ArticleListViewResponse.java(DTO)
    => view에게 데이터를 전달하기 위한 객체 생성
    
    - article.html, newArticle.html(view)
    => 글 게시 시간 표시, 생성, 수정, 삭제 기능 구현
    
    - articel.js
    => 글 생성, 수정, 삭제 버튼과 팝업 기능 구현
    
    2. 수정
    - Article.java
    => 엔티티에 글 생성, 수정 시간 추가.
    
    -data.sql
    =>서버 실행 시 마다 글의 생성, 수정 시간 업데이트 되도록 설정 변경.
    
    -SpringBootDeveloperApplication.java
    => 글 생성, 수정 시간 자동 업데이트 되도록 annotation 추가

--- src/main/java/sjspring/shop/springbootdeveloper/controller/BlogVewController.java
+++ src/main/java/sjspring/shop/springbootdeveloper/controller/BlogVewController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import sjspring.shop.springbootdeveloper.domain.Article;
 import sjspring.shop.springbootdeveloper.dto.ArticleListViewResponse;
 import sjspring.shop.springbootdeveloper.dto.ArticleViewResponse;
@@ -37,6 +38,19 @@ public class BlogVewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    // 쿼리파라미터로 넘어온 id값을 newArticle() Long 타입 id 인자에 매핑한다.(id는 없을 수도 있음)
+    public String newArticle(@RequestParam(required = false) Long id, Model model)
+    {
+        if(id ==null){ //id가 없으면 새로 생성한다.
+            model.addAttribute("article", new ArticleViewResponse());
+        }else{ //id가 있으면 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
     }
 
 }
